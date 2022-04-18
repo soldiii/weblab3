d6dedfec0dc0c9821a896a614bd4ef084b724003
MikhailPolivoda <71961349+MikhailPolivoda@users.noreply.github.com>
2022-04-18 14:38:41 +0700

    Update app.js
    сократил код

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 async function go() {
     let vacancies = document.getElementById("input").value;
-    let request = `https://api.hh.ru/vacancies?only_with_salary=true&per_page=100&page=1&text=${vacancies}`;
+    let request = `https://api.hh.ru/vacancies?per_page=100&page=1&text=${vacancies}`;
     let response = await fetch(request).then(response => response.json()).then(data => {
        let vac='';
         for(let i = 0; i<data.items.length;i++)
         {
             let salary = ""
-            let valuta=data.items[i].salary.currency
-            if(valuta==='RUR'){valuta="RUB"}
-            if(data.items[i].salary.from!=null && data.items[i].salary.to!=null)
+            let qq=data.items[i].salary
+            if(data.items[i].salary==null)
             {
-                salary = "от "+data.items[i].salary.from+" до "+data.items[i].salary.to+" "+valuta
+                salary=' не указана'
             }
-            else if (data.items[i].salary.from ==null && data.items[i].salary.to==null)
+            else
             {
-                salary = "Не указана";
-            }
-            else if(data.items[i].salary.from==null && data.items[i].salary.to!=null)
-            {
-                salary = " до "+data.items[i].salary.to+" "+valuta;
-            }
-            else if(data.items[i].salary.from!=null && data.items[i].salary.to==null)
-            {
-                salary = " от "+data.items[i].salary.from+" "+valuta;
+                let valuta = data.items[i].salary.currency
+
+                if (qq.from == qq.to)
+                {
+                    salary = qq.from
+                }
+                else
+                {
+                    if (qq.from != null)
+                    {
+                        salary += "от " + qq.from
+                    }
+                    if (qq.to != null)
+                    {
+                        salary += " до " + qq.to
+                    }
+                }
+
+                if (valuta === 'RUR') {
+
+                    salary += " RUB"
+                }
+                else
+                {
+                    salary +=" "+ valuta
+                }
+
             }
             vac +="Вакансия №"+(i+1)+ "\nНазвание вакансии: "+data.items[i].name +"\nКомпания: "
                 +data.items[i].employer.name+"\nГород: "+data.items[i].area.name+"\nЗарплата: "
